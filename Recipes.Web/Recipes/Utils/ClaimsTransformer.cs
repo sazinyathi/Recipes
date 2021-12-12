b9d26d0399f6d952664b53426e545a193dcd8105
sazin <sazin@tritonexpress.co.za>
2021-12-12 07:59:27 +0200

    Code CleanUp

--- Recipes.Web/Recipes/Utils/ClaimsTransformer.cs
+++ Recipes.Web/Recipes/Utils/ClaimsTransformer.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Configuration;
 using Recipes.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
